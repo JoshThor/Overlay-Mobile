edde517aa6cca27cc34e14f012597929eb3aca79
Josh Thorson <joshthorson5@gmail.com>
2016-11-14 20:57:58 -0500

    Loads .obj models from a URL

--- Assets/Vuforia/Scripts/CloudRecoEventHandler.cs
+++ Assets/Vuforia/Scripts/CloudRecoEventHandler.cs
@@ -7,6 +7,9 @@ Confidential and Proprietary - QUALCOMM Austria Research Center GmbH.
 using UnityEngine;
 using System.Collections;
 using Vuforia;
+using System.Collections.Generic;
+using System;
+using System.IO;
 
 public class CloudRecoEventHandler : MonoBehaviour, ICloudRecoEventHandler
 {
@@ -14,7 +17,23 @@ public class CloudRecoEventHandler : MonoBehaviour, ICloudRecoEventHandler
     private CloudRecoBehaviour mCloudRecoBehaviour;
     private bool mIsScanning = false;
 
+    //Bundle URL;
+    private string modelURL = "http://people.sc.fsu.edu/~jburkardt/data/obj/cube.obj";
+
+    /* Models that i know work:
+    Cube: http://people.sc.fsu.edu/~jburkardt/data/obj/cube.obj
+    Decahedron: http://people.sc.fsu.edu/~jburkardt/data/obj/dodecahedron.obj
+    Pyramid: http://people.sc.fsu.edu/~jburkardt/data/obj/pyramid.obj //Note: you have to modify ObjImporter line 45 to: newVerts[i] = newMesh.vertices[(int)v.x - 1 -1];
+    Tetrahedron: http://people.sc.fsu.edu/~jburkardt/data/obj/tetrahedron.obj
+    Humanoid *Works but is  a bit large*: http://people.sc.fsu.edu/~jburkardt/data/obj/humanoid_quad.obj
+     */
+
     public ImageTargetBehaviour ImageTargetTemplate;
+
+    public GameObject emptyPrefabWithMeshRenderer;
+
+    public Material renderMaterial;
+
 
     // Use this for initialization
     void Start()
@@ -34,34 +53,32 @@ public class CloudRecoEventHandler : MonoBehaviour, ICloudRecoEventHandler
 
     public void OnInitError(TargetFinder.InitState initError)
     {
-        // handle error
+    // handle error
     }
 
     public void OnUpdateError(TargetFinder.UpdateState updateError)
     {
-        // handle error
+    // handle error
     }
 
     public void OnStateChanged(bool scanning)
     {
-        mIsScanning = scanning;
+    mIsScanning = scanning;
 
-        if (scanning)
+    if (scanning)
+    {
+        // clear all known trackables
+        ObjectTracker tracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+        tracker.TargetFinder.ClearTrackables(false);
+
+        // Remove the augmentation (cube object)
+        if (ImageTargetTemplate)
         {
-            // clear all known trackables
-            ObjectTracker tracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
-            tracker.TargetFinder.ClearTrackables(false);
-
-            // Remove the augmentation (cube object)
-            if (ImageTargetTemplate)
-            {
-                Transform cube = ImageTargetTemplate.transform.FindChild("MyAugmentation");
-                if (cube)
-                {
-                    GameObject.Destroy(cube.gameObject);
-                }
-            }
+            //Destroys any active 3D objects
+            if (ImageTargetTemplate.transform.childCount > 0)
+                Destroy(ImageTargetTemplate.transform.GetChild(0).gameObject);
         }
+    }
     }
 
     // Here we handle a target reco event
@@ -70,27 +87,79 @@ public class CloudRecoEventHandler : MonoBehaviour, ICloudRecoEventHandler
         // stop the target finder
         mCloudRecoBehaviour.CloudRecoEnabled = false;
 
+        if(targetSearchResult.MetaData == null)
+        {
+            return;
+        }
+
+        //Fetch the Bundle URL from the metadata
+        //modelURL = targetSearchResult.MetaData;
+
         // Build augmentation based on target
         if (ImageTargetTemplate)
         {
             // enable the new result with the same ImageTargetBehaviour:
             ObjectTracker tracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
             ImageTargetBehaviour imageTargetBehaviour = tracker.TargetFinder.EnableTracking(targetSearchResult, ImageTargetTemplate.gameObject) as ImageTargetBehaviour;
+            Debug.Log("Image Recognized");
 
-            // Create a simple cube
-            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube.name = "MyAugmentation";
+           // GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+           // cube.transform.localPosition = Vector3.zero;
+           // cube.transform.localScale = Vector3.one * 20;
+           // cube.transform.localRotation = Quaternion.identity;
+            StartCoroutine("DownloadObject");
+        }
+    }
 
-            // Attach cube as child of the cloud image target
-            cube.transform.parent = imageTargetBehaviour.gameObject.transform;
+    IEnumerator DownloadObject()
+    {
+        //Destroys any active 3D objects
+        if (ImageTargetTemplate.transform.childCount > 0)
+            Destroy(ImageTargetTemplate.transform.GetChild(0).gameObject);
 
-            // Setup local transformation
-            cube.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
-            cube.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-            cube.transform.localRotation = Quaternion.identity;
+        // Load the 3D Object file from the specified URL
+        using (WWW www = new WWW(modelURL))
+        {
+            yield return www;
+            if (www.error != null)
+                throw new UnityException("WWW download had an error:" + www.error);
 
-            // Enable cube
-            cube.SetActive(true);
+            string fileText = www.text;
+
+
+            GameObject spawnedPrefab;
+            Mesh importedMesh = ObjImporter.ImportFile(fileText, "ARO");
+            spawnedPrefab = Instantiate(emptyPrefabWithMeshRenderer, transform.position, transform.rotation) as GameObject;
+            spawnedPrefab.GetComponent<MeshFilter>().mesh = importedMesh;
+            spawnedPrefab.GetComponent<Renderer>().material = renderMaterial;
+            spawnedPrefab.transform.parent = ImageTargetTemplate.transform;
+            spawnedPrefab.transform.localPosition = Vector3.zero;
+            spawnedPrefab.transform.localScale = Vector3.one * 1;
+            spawnedPrefab.transform.localRotation = Quaternion.identity;
+
+            Debug.Log("Model Loaded");
+
+            List<string> lines = new List<string>(fileText.Split(new string[] { "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries));
+
+            using (StreamWriter sw = new StreamWriter("TestFile.txt"))
+            {
+                foreach (string s in lines)
+                {
+                    sw.WriteLine(s);
+                }
+            }
+
+            /*GameObject loaded = OBJLoader.LoadOBJFile(lines, "ARO");
+            //foreach (Transform child in ImageTargetTemplate.transform)
+          //  {
+          //      Destroy(child);
+          //  }
+            loaded.transform.parent = ImageTargetTemplate.transform;
+
+            loaded.transform.localPosition = Vector3.zero;
+            loaded.transform.localScale = Vector3.one * 20;
+            loaded.transform.localRotation = Quaternion.identity;
+            */
         }
     }
 
